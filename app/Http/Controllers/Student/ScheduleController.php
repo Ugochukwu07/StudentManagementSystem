c771f9d77d91205aa8174f713d157f046c7f4c61
Ekwueme Ugochukwu <ekwuemeugochukwu83@gmail.com>
2023-03-20 23:03:58 +0100

    finised up with schedule

--- app/Http/Controllers/Student/ScheduleController.php
+++ app/Http/Controllers/Student/ScheduleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
 use App\Models\Schedule;
+use App\Service\Student\ScheduleService;
 use Illuminate\Http\Request;
 
 class ScheduleController extends Controller
@@ -12,8 +13,14 @@ class ScheduleController extends Controller
 
     public function schedules()
     {
-        $schedules = Schedule::where('student_id', $this->id())->get();
+        $schedules = (new ScheduleService())->getSchedules($this->id());
 
         return view('student.schedule.index', compact('schedules'));
+    }
+
+    public function details($id)
+    {
+        $schedule = Schedule::find($id);
+        return view('student.schedule.details', compact('schedule'));
     }
 }
---
+++ app/Service/Student/ScheduleService.php
@@ -0,0 +1,78 @@
+<?php
+namespace App\Service\Student;
+
+use App\Models\User;
+use App\Models\Profile;
+use App\Models\Schedule;
+use Illuminate\Support\Collection;
+
+class ScheduleService{
+    /**
+     * sort schedules By Time Start
+     *
+     * @param  array $data
+     * @return Collection
+     */
+    public function sortByTime(array $data): Collection
+    {
+        $data = collect($data);
+        $sorted = $data->sortByDesc(function($datum){
+            return strtotime($datum->start_time);
+        });
+        return $sorted;
+    }
+
+
+    /**
+     * get one student Schedules
+     *
+     * @param  User $user_id
+     * @return array
+     */
+    public function getSchedules(User $user_id):array
+    {
+        $profile = Profile::where('user_id', $user_id)->first();
+
+        $schedules = Schedule::where('session_id', $profile->session_id)->where('department_id', $profile->department_id)->get();
+        $monday = $tuesday = $wednesday = $thursday = $friday = $saturday = $sunday = array();
+        foreach($schedules as $schedule){
+            switch ($schedule->day) {
+                case 1:
+                    array_push($monday, $schedule);
+                    break;
+                case 2:
+                    array_push($tuesday, $schedule);
+                    break;
+                case 3:
+                    array_push($wednesday, $schedule);
+                    break;
+                case 4:
+                    array_push($thursday, $schedule);
+                    break;
+                case 5:
+                    array_push($friday, $schedule);
+                    break;
+                case 6:
+                    array_push($saturday, $schedule);
+                    break;
+                case 7:
+                    array_push($sunday, $schedule);
+                    break;
+
+                default:
+                    continue;
+                    break;
+            }
+        }
+
+        $weekdays = compact('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday');
+
+        $schedules = [];
+        foreach($weekdays as $key => $weekday){
+            $schedules[$key] = $this->sortByTime($weekday);
+        }
+
+        return $schedules;
+    }
+}
+
